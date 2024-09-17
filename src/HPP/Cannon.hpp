ab65449c8004c612e11570d014852265a6f61641
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-17 15:20:42 +0200

    Create class Cannon who inherits GameObject class

---
+++ src/HPP/Cannon.hpp
@@ -0,0 +1,18 @@
+#ifndef CANNON_HPP
+#define CANNON_HPP
+
+#include "GameObject.hpp"
+
+class Cannon : public GameObject{
+    private:
+        float angle;
+
+    public:
+        Cannon(sf::Vector2f position, const sf::Texture& texturePath);
+        ~Cannon();
+        void rotate(sf::RenderWindow& window);
+        float getAngle();
+
+};
+
+#endif
