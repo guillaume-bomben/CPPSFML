eee40a0de812bf74887dbc3397012b1e7071bbd3
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-17 15:03:50 +0200

    change constructor declaration

--- src/CPP/Brick.cpp
+++ src/CPP/Brick.cpp
@@ -1,5 +1,5 @@
 #include "../HPP/Brick.hpp"
 
-Brick::Brick(sf::Vector2f position, sf::Texture TexturePath) : GameObject(position, this->size, TexturePath){}
+Brick::Brick(sf::Vector2f position, const sf::Texture& texturePath) : GameObject(position, sf::Vector2f(3, 2), texturePath){}
 
 Brick::~Brick(){}
--- src/HPP/Brick.hpp
+++ src/HPP/Brick.hpp
@@ -4,11 +4,9 @@
 #include "GameObject.hpp"
 
 class Brick : public GameObject{
-    private:
-        sf::Vector2f size = sf::Vector2f(3, 2);
 
     public:
-        Brick(sf::Vector2f position, sf::Texture TexturePath);
+        Brick(sf::Vector2f position, const sf::Texture& texturePath);
         ~Brick();
 };
 
