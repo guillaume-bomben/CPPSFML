a8a4dd0f44ad7ecec23c07ba2a1ff0d524081137
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-17 15:21:18 +0200

    change size of brick

--- src/CPP/Brick.cpp
+++ src/CPP/Brick.cpp
@@ -1,5 +1,5 @@
 #include "../HPP/Brick.hpp"
 
-Brick::Brick(sf::Vector2f position, const sf::Texture& texturePath) : GameObject(position, sf::Vector2f(3, 2), texturePath){}
+Brick::Brick(sf::Vector2f position, const sf::Texture& texturePath) : GameObject(position, sf::Vector2f(1, 1), texturePath){}
 
 Brick::~Brick(){}
