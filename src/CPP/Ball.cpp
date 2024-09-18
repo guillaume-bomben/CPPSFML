cdcec02ef97d0a7cd3c25c0bba1b058d97415822
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-18 08:23:31 +0200

    add method to check collision between the ball and the bricks as well as the edges of the screen except the bottom and delete brick if is touch

--- src/CPP/Ball.cpp
+++ src/CPP/Ball.cpp
@@ -8,3 +8,23 @@ void Ball::update(){
     this->position += this->velocity;
     this->sprite.setPosition(this->position);
 }
+
+void Ball::isColliding(std::vector<Brick>& brick, sf::RenderWindow& window){
+    // Utilisation d'un itérateur pour parcourir le vecteur et supprimer les briques
+    for (auto it = brick.begin(); it != brick.end(); ){
+        if (this->sprite.getGlobalBounds().intersects(it->getSprite().getGlobalBounds())){
+            this->velocity.y = -this->velocity.y;
+            it = brick.erase(it);  // Supprime la brique et met à jour l'itérateur
+        } else {
+            ++it;  // Avance l'itérateur seulement si pas de suppression
+        }
+    }
+
+    // Gestion des collisions avec les bords de la fenêtre
+    if (this->position.x < 0 || this->position.x > window.getSize().x){
+        this->velocity.x = -this->velocity.x;
+    }
+    else if (this->position.y < 0 ){
+        this->velocity.y = -this->velocity.y;
+    }
+}
--- src/HPP/Ball.hpp
+++ src/HPP/Ball.hpp
@@ -2,16 +2,17 @@
 #define BALL_HPP
 
 #include "GameObject.hpp"
+#include "Brick.hpp"
 
 class Ball : public GameObject{
     private:
-        sf::Vector2f velocity = sf::Vector2f(0.1, 0.1);
+        sf::Vector2f velocity = sf::Vector2f(0.5, 0.5);
 
     public:
         Ball(sf::Vector2f position, sf::Texture TexturePath);
         ~Ball();
         void update();
-        
+        void isColliding(std::vector<Brick>& brick, sf::RenderWindow& window);
 };
 
 #endif
